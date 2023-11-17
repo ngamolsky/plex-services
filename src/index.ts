912e3e84a1a99df72afdfb991c41c6b9dd4f8abc
Nikita Gamolsky <nikita.gamolsky@gmail.com>
2023-11-16 21:50:17 -0800

    start working on form

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,10 @@ import { addPlexRequest } from './notion';
 export interface Env {
 	NOTION_INTEGRATION_KEY: string;
 	NOTION_PLEX_REQUEST_DATABASE_ID: string;
-	PLEX_PASSPHRASE_ANSWER: string;
-	ENVIRONMENT: 'production' | 'development';
-	SENDGRID_API_KEY: string;
-	SENDGRID_FROM_EMAIL: string;
+	PLEX_PASSPHRASE_ANSWER_OPTIONS: string;
 	SENDGRID_TO_EMAIL: string;
 	SEND_EMAIL: Fetcher;
+	WORKER_ENV?: 'local';
 }
 
 const NEW_REQUEST_PATH = '/plex/new-request';
@@ -16,6 +14,8 @@ const NEW_REQUEST_PATH = '/plex/new-request';
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const requestPath = new URL(request.url).pathname;
+
+		console.log(`Request path: ${requestPath}`, env);
 
 		// Only allow POST requests
 		if (request.method !== 'POST') {
@@ -36,7 +36,7 @@ export default {
 const addNewPlexRequest = async (request: Request, env: Env) => {
 	const apiKey = env.NOTION_INTEGRATION_KEY;
 	const databaseId = env.NOTION_PLEX_REQUEST_DATABASE_ID;
-	const correctPassphrase = env.PLEX_PASSPHRASE_ANSWER;
+	const correctPassphraseOptions = env.PLEX_PASSPHRASE_ANSWER_OPTIONS.split(', ');
 	const body = (await request.json()) as { title: string; why: string; who: string; passphrase: string; email?: string };
 
 	const title = body.title;
@@ -51,8 +51,16 @@ const addNewPlexRequest = async (request: Request, env: Env) => {
 
 	const headers = new Headers(request.headers);
 
+	// Allow CORS on localhost
+
+	if (env.WORKER_ENV === 'local') {
+		headers.set('Access-Control-Allow-Origin', 'http://localhost:8000');
+	}
+
+	const lowerCaseCorrectPassphraseOptions = correctPassphraseOptions.map((option) => option.toLowerCase());
+
 	// Passphrase check is case insensitive
-	if (passphrase.toLowerCase() !== correctPassphrase.toLowerCase()) {
+	if (lowerCaseCorrectPassphraseOptions.indexOf(passphrase.toLowerCase()) === -1) {
 		return new Response('Incorrect passphrase', {
 			status: 401,
 			headers: headers,
@@ -61,7 +69,9 @@ const addNewPlexRequest = async (request: Request, env: Env) => {
 
 	await addPlexRequest(apiKey, databaseId, title, why, who, email);
 
-	const result = await env.SEND_EMAIL.fetch('https://send-email.ng-cloudflare.workers.dev/', {
+	const sendEmailUrl = env.WORKER_ENV === 'local' ? 'http://localhost:54825' : 'https://send-email.ng-cloudflare.workers.dev/';
+
+	const result = await env.SEND_EMAIL.fetch(sendEmailUrl, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
